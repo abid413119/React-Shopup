3f98d867d8b1586d4c1b5bda5eaa4a2ada2fc30c
abid413119 <abidastronaut@gmail.com>
2020-01-17 23:47:03 +0600

    Updates

---
+++ precache-manifest.8c0c1697e15cb7cde137fe32fbebbde2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccf12688645741927abc",
+    "url": "/React-Shopup/static/js/runtime~main.baa80474.js"
+  },
+  {
+    "revision": "6660b19a82941e583a7b",
+    "url": "/React-Shopup/static/js/main.485fcfbf.chunk.js"
+  },
+  {
+    "revision": "05db2bca1d67b827c309",
+    "url": "/React-Shopup/static/js/2.1714966f.chunk.js"
+  },
+  {
+    "revision": "6660b19a82941e583a7b",
+    "url": "/React-Shopup/static/css/main.9e0046e0.chunk.css"
+  },
+  {
+    "revision": "05db2bca1d67b827c309",
+    "url": "/React-Shopup/static/css/2.86220c1e.chunk.css"
+  },
+  {
+    "revision": "a006251e04b4693fd71b9aee8c1256ef",
+    "url": "/React-Shopup/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/React-Shopup/precache-manifest.8c0c1697e15cb7cde137fe32fbebbde2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/React-Shopup/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
